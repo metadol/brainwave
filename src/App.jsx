361abafe28af69d5a47c3baf42553094569b308c
Kevin Abraham <kevingitabr@gmail.com>
2025-01-04 15:21:36 +0530

    added reusable button component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
-export default function App() {
+import ButtonGradient from './assets/svg/ButtonGradient'
+import Button from './components/Button'
+
+const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+      <h1 className="text-3xl">Heading</h1>
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <Button className="mt-20" href="#login">
+          Something
+        </Button>
+        <ButtonGradient />
+      </div>
+
+    </>
   )
 }
+
+export default App
